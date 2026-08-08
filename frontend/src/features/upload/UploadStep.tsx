7bec9888612d9263be4f53bf76d03898a39b4b0d
Saravanan Sivalingam <saravanan.exult@gmail.com>
2026-08-08 17:39:12 +0530

    Change upload icon

--- frontend/src/features/upload/UploadStep.tsx
+++ frontend/src/features/upload/UploadStep.tsx
@@ -139,7 +139,56 @@ export const UploadStep: React.FC<UploadStepProps> = ({ onUploadSuccess }) => {
         onDragLeave={handleDragLeave}
         onDrop={handleDrop}
       >
-        <div className="dropzone-icon">📁</div>
+        <div className="dropzone-icon">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+            width="100px"
+            height="100px"
+            role="img"
+            aria-labelledby="title desc"
+          >
+            <title id="title">Excel document icon</title>
+            <desc id="desc">
+              A green circular icon with a white XLS spreadsheet document.
+            </desc>
+            <defs>
+              <clipPath id="iconClip">
+                <circle cx="256" cy="256" r="240" />
+              </clipPath>
+            </defs>
+            <g clip-path="url(#iconClip)">
+              <rect width="512" height="512" fill="#45be63" />
+              <path
+                fill="#fff"
+                d="M160 112h144c66.3 0 120 53.7 120 120v184c-31.5 51.7-91.6 80-168 80s-136.5-28.3-168-80V184c0-39.8 32.2-72 72-72Z"
+              />
+
+              <path
+                fill="#d9f2e0"
+                d="M304 112c66.3 0 120 53.7 120 120v24c0-79.5-40.3-136-120-144Z"
+              />
+
+              <path
+                fill="#16a43a"
+                d="M113 209h16.4l11.9 37.5 12.1-37.5h16.2l-20.2 51 21.5 50h-16.7l-13-38.4-12.8 38.4H112l21-50.1L113 209Zm64.3 0h15.8v84.7h29.1V310h-44.9v-101Zm70.2-1c15.6 0 24.7 8.2 24.7 25.1v6.3h-15.7v-7.8c0-5.9-2.8-8.6-8.3-8.6-5.2 0-8.2 2.6-8.2 8.3 0 6.7 4.5 11.1 14.4 18.2 12.5 9.2 18.4 17.3 18.4 31.5 0 19.4-9.4 30.4-25.6 30.4-15.8 0-25.2-9.4-25.2-27.6v-6.5h15.8v7.7c0 7.1 2.8 10.3 9.1 10.3 5.9 0 9.2-3.7 9.2-10.8 0-7.7-4.8-13.1-14.5-20.1-13.9-10.1-18.6-17.9-18.6-30.2 0-16.2 9.4-25.2 25.5-25.2Z"
+              />
+
+              <rect
+                x="112"
+                y="329"
+                width="288"
+                height="114"
+                rx="18"
+                fill="#d9f2e0"
+              />
+              <path
+                fill="#fff"
+                d="M130 346h117v30H130zm135 0h118v30H265zm-135 48h117v29H130zm135 0h118v29H265z"
+              />
+            </g>
+          </svg>
+        </div>
         <h3>Drag & Drop your campaign files here</h3>
         <p>Supports CSV, XLSX, and XLS performance files up to 50MB</p>
 
